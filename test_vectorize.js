f54e4e81492a897a9db5f42a50021412fe8b6a5e
Ben Anhalt <ben.anhalt@gmail.com>
2010-05-23 01:39:04 -0500

    passing some basic tests

--- test_vectorize.js
+++ test_vectorize.js
@@ -1,27 +1,61 @@
-  $(document).ready(function(){
+$(function(){
+    var edgeSet = {};
 
-test("a basic test example", function() {
-  ok( true, "this test is fine" );
-  var value = "hello";
-  equals( "hello", value, "We expect value to be hello" );
-});
+    module("addBlock");
 
-module("Module A");
+    test("add one block", function() {
+	   Vectorize.addBlock(edgeSet, 0,0);
+	   equal(edgeSet["0,0 0,1"], 1);
+	   equal(edgeSet["0,1 1,1"], 1);
+	   equal(edgeSet["1,1 1,0"], 1);
+	   equal(edgeSet["1,0 0,0"], 1);
+	   var count = 0;
+	   for (var edge in edgeSet) count++;
+	   equal(count, 4);
+	 });
 
-test("first test within module", function() {
-  ok( true, "all pass" );
-});
+    test("three blocks", function () {
+	   Vectorize.addBlock(edgeSet, 1,0);
+	   Vectorize.addBlock(edgeSet, 1,1);
+	   equal(edgeSet["0,0 0,1"], 1);
+	   equal(edgeSet["0,1 1,1"], 1);
+	   equal(edgeSet["1,1 1,2"], 1);
+	   equal(edgeSet["2,2 2,1"], 1);
+	   equal(edgeSet["2,1 2,0"], 1);
+	   equal(edgeSet["2,0 1,0"], 1);
+	   equal(edgeSet["1,0 0,0"], 1);
+	   var count = 0;
+	   for (var edge in edgeSet) count++;
+	   equal(count, 8);
+	 });
 
-test("second test within module", function() {
-  ok( true, "all pass" );
-});
+    module("findVertices");
 
-module("Module B");
+    test("six vertices", function () {
+	   var vertices = [];
+	   var orientation = 0;
+	   var paths = 0;
+	   function foundVertex(v, direction) {
+	     vertices.push(v);
+	     direction == "left" ? orientation++ : orientation--;
+	   }
+	   function pathComplete() {
+	     paths++;
+	   }
 
-test("some other test", function() {
-  expect(2);
-  equals( true, false, "failing test" );
-  equals( true, true, "passing test" );
-});
+	   Vectorize.findVertices(edgeSet, foundVertex, pathComplete);
+	   equal(paths, 1);
+	   ok(orientation > 0, "Orientation is positive for the outside boundary.");
+	   equal(vertices.length, 6, "Should be six vertices.");
 
+	   function join(v) {return v.join(",");}
+	   vertices = vertices.map(join);
+
+	   var i = vertices.indexOf("0,0");
+	   ok(i != -1, "0,0 is a vertex. Index: " + i);
+	   var expected = ["0,0", "0,1", "1,1", "1,2", "2,2", "2,0"];
+	   for (var j = 0; j < 6; j++) {
+	     equal(expected[j], vertices[(i+j)%6]);
+	   }
+	 });
 });
--- vectorize.js
+++ vectorize.js
@@ -2,33 +2,6 @@ var Vectorize = function () {
 
   var DIRECTIONS = ["1,0", "0,-1", "-1,0", "0,1"];
 
-  function getEdges(blocks) {
-    var edgeSet = {};
-    for (var i in blocks) {
-      var b = blocks[i];
-      addBlock(edgeSet, b[0], b[1]);
-    }
-    return edgeSet;
-  }
-
-  function addBlock(edgeSet, x, y) {
-    var edges = [
-      [[x, y], [x, y+1]],
-      [[x, y+1], [x+1, y+1]],
-      [[x+1, y+1], [x+1, y]],
-      [[x+1, y], [x, y]]
-    ];
-
-    for (var j in edges) {
-      var edge = edges[j];
-      var reversed = edge[1].join(",") + " " + edge[0].join(",");
-      if (edgeSet[reversed] == undefined)
-	edgeSet[edge[0].join(",") + " " + edge[1].join(",")] = 1;
-      else
-	delete edgeSet[reversed];
-    }
-  }
-
   function sub(a, b) {
     return [b[0] - a[0], b[1] - a[1]];
   }
@@ -51,18 +24,30 @@ var Vectorize = function () {
     return str.split(",").map(function(s){return parseInt(s, 10);});
   }
 
-  function fromEdges(edges) {
-    var boundaries = [];
+  function addBlock (edgeSet, x, y) {
+    var edges = [
+      [[x, y], [x, y+1]],
+      [[x, y+1], [x+1, y+1]],
+      [[x+1, y+1], [x+1, y]],
+      [[x+1, y], [x, y]]
+    ];
 
+    for (var j in edges) {
+      var edge = edges[j];
+      var reversed = edge[1].join(",") + " " + edge[0].join(",");
+      if (edgeSet[reversed] == undefined)
+	edgeSet[edge[0].join(",") + " " + edge[1].join(",")] = 1;
+      else
+	delete edgeSet[reversed];
+    }
+  }
+
+  function findVertices (edges, vertexFound, pathComplete) {
     while(true) {
       var start = "";
       for (start in edges) {break;}
       if (start == "") break;
       delete edges[start];
-
-      var boundary = [];
-      //    boundary.orientation = 0;
-      boundaries[boundaries.length] = boundary;
 
       var edge = start;
       while(true) {
@@ -82,12 +67,15 @@ var Vectorize = function () {
           continue;
 	}
 
-	boundary[boundary.length] = ends[1];
-
 	o = splitToInt(turnLeft(os));
 	adjacentEdge = ends[1] + " " + add(v, o).join(",");
+
+	if (adjacentEdge == start) {
+	  vertexFound(v, "left");
+	  break;
+	}
 	if (edges[adjacentEdge] != undefined) {
-	  //      boundary.orientation += 1;
+	  vertexFound(v, "left");
           edge = adjacentEdge;
           delete edges[edge];
           continue;
@@ -95,15 +83,25 @@ var Vectorize = function () {
 
 	o = splitToInt(turnRight(os));
 	adjacentEdge = ends[1] + " " + add(v, o).join(",");
+
+	if (adjacentEdge == start) {
+	  vertexFound(v, "right");
+	  break;
+	}
 	if(edges[adjacentEdge] != undefined) {
-	  //      boundary.orientation -= 1;
+	  vertexFound(v, "right");
           edge = adjacentEdge;
           delete edges[edge];
           continue;
 	}
 	break;
       }
+      pathComplete();
     }
-    return boundaries;
   }
+
+  return {
+    addBlock: addBlock,
+    findVertices: findVertices
+  };
 } ();
